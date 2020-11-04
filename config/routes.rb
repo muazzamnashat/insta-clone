87c483830477a47c40e7be7e8660cae088308d54
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-11-04 15:33:12 -0500

    work on github login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { registrations: "registrations", omniauth_callbacks: "callbacks" }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "public#index"
+  root to: "sessions#index"
+  match "/auth/github/callback", to: "sessions#create", via: [:get, :post]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_04_171947) do
+ActiveRecord::Schema.define(version: 2020_11_04_203218) do
 
   create_table "users", force: :cascade do |t|
     t.string "username", limit: 20
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 2020_11_04_171947) do
     t.string "unconfirmed_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
