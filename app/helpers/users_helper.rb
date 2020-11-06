59b86031d21cf89056a91adcc0684c079a597d7c
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-11-05 23:14:52 -0500

    work on Profile view page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,4 +3,8 @@ module UsersHelper
     image_path = user.image.thumb.url || "profile-placeholder.jpg"
     image_tag(image_path, width: width, class: "img-circle")
   end
+
+  def can_edit_profile?(user_id)
+    user_signed_in? && current_user.id == user_id
+  end
 end
