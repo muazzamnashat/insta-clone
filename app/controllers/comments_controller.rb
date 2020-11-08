5ef35d1fa2768dbad2e1e2c54b214d7aeb4b5e65
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-11-08 12:15:42 -0500

    fix post show page redirect

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,7 +6,7 @@ class CommentsController < ApplicationController
     @comment.user_id = current_user.id if user_signed_in?
     if @comment.save
       # added this extra hidden return_to field only to the show page to make sure the comment request is goin from show page so we redirect user to same page
-      return_url = params[:comment][:return_to].present? ? post_path(@comment.post_id) : posts_path
+      return_url = params[:comment][:return_to].present? ? user_post_path(@comment.user_id, @comment.post_id) : posts_path
       redirect_to return_url, flash: { success: "Comment was created successfully!" }
     else
       redirect_to posts_path, flash: { danger: "Post was not saved!" }
