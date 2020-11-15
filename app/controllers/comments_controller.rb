014dfd96ee17e46a0f5edd28f97c736ccfb5db00
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-11-15 03:10:47 -0500

    add pluralize to likes count

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,9 +9,9 @@ class CommentsController < ApplicationController
     if @comment.save
       # added this extra hidden return_to field only to the show page to make sure the comment request is goin from show page so we redirect user to same page
       return_url = params[:comment][:return_to].present? ? user_post_path(@comment.user_id, @comment.post_id) : posts_path
-      redirect_to return_url, flash: { success: "Comment was created successfully!" }
+      redirect_to return_url
     else
-      redirect_to posts_path, flash: { danger: "Post was not saved!" }
+      redirect_to posts_path
     end
   end
 
