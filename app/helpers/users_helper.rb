0ddde5a27f359561e8344c3ff21f595bff4993f5
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-11-19 14:01:12 -0500

    fix dp sizing

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
-  def profile_picture(user, width = 100)
+  def profile_picture(user, width = 100, height = nil)
     image_path = user.image.url || "profile-placeholder.jpg"
-    image_tag(image_path, width: width, class: "img-circle m-r-5")
+    image_tag(image_path, width: width, height: height, class: "img-circle m-r-5")
   end
 
   def can_edit_profile?(user_id)
