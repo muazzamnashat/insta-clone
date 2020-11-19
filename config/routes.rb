97bfefb27aac1f9a7a669b20086dca6a07746c9c
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-11-19 09:50:35 -0500

    change root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: "registrations", omniauth_callbacks: "callbacks" }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "public#homepage"
-  # match "/auth/github/callback", to: "registrations#create", via: [:get, :post]
+
+  root to: "posts#index"
   get "/users/:username", to: "users#profile", as: :profile
   get "/post/like/:post_id", to: "likes#save_like", as: :like_post
   post "/follow/account", to: "users#follow_account", as: :follow_account
+
   resources :users do
     resources :posts, only: [:show, :create, :new, :edit, :destroy, :update]
   end
+
   resources :posts, only: [:index]
   resources :comments, only: [:create, :destroy]
 
