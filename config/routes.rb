988022ce45e6b980f8e9799a529c4ee11477e6ef
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-12-22 05:01:35 -0500

    disabel omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { registrations: "registrations", omniauth_callbacks: "callbacks" }
+  # devise_for :users, :controllers => { registrations: "registrations", omniauth_callbacks: "callbacks" }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, :controllers => { registrations: "registrations" }
 
   root to: "posts#index"
   get "/users/:username", to: "users#profile", as: :profile
