dcfb62ee69089e89a101858b41a8e537a34017b6
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-12-22 05:52:13 -0500

    add local storage

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   # changed for heroku
-  config.active_storage.service = :local
+  # config.active_storage.service = :local
 
   # config.active_storage.service = :google
 
