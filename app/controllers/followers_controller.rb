428c1c06ca3c42ac14494d0d8a950f8be96a8473
Steven Inouye <hiiamsteveni@gmail.com>
2020-12-26 16:35:24 -0800

    commit

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -2,10 +2,10 @@ class FollowersController < ApplicationController
 
   # get a followers list for a particle user
   def followers_list
-    @users = User.find_by_id(params[:id]).followers
+    @users = User.find(params[:id]).followers
   end
 
   def following_list
-    @users = User.find_by_id(params[:id]).followings
+    @users = User.find(params[:id]).followings
   end
 end
